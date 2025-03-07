4b3d6baa0e89a2d2a7706ad543c3ec6c8d595f34
philip <philip.deric@ti.ukdw.ac.id>
2025-03-07 14:45:41 +0700

    switch scene to login added

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-import java.lang.ModuleLayer.Controller;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/RegistrationController.java
+++ src/RegistrationController.java
@@ -1,10 +1,16 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import java.io.IOException;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class RegistrationController {
-
     @FXML
     private Button mulai_input;
 
@@ -15,6 +21,9 @@ public class RegistrationController {
     private TextField password_input;
 
     @FXML
+    private Hyperlink login;
+
+    @FXML
     void ClickedMulai() {
         String name = name_input.getText();
         String pass = password_input.getText();
@@ -22,5 +31,25 @@ public class RegistrationController {
         System.out.println(name);
         System.out.println(pass);
     }
+
+    Stage getStage(ActionEvent e) {
+        /* Mendapatkan Stage dari node objek e yang di lakukan action*/ 
+        return (Stage) ( ((Node) e.getSource()).getScene().getWindow() );
+    }
+
+    @FXML
+    void ClickedLoginDisini(ActionEvent e) throws IOException {
+        /* User yang sudah memiliki akun */
+        System.out.println("Login clicked");
+
+        FXMLLoader fxml_load = new FXMLLoader(getClass().getResource("Login.fxml"));
+        Parent root = fxml_load.load();
+        
+        Stage currStage = getStage(e);
+        currStage.setScene(new Scene(root));
+        currStage.show();
+
+    }
+
 
 }
